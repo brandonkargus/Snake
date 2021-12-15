9f9f20274212701f3903cc5cf445fc18b721a9d2
Brandon <brandonkargus@gmail.com>
2021-12-15 08:30:50 -0600

    Added Snake to online repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Retro "snake" game influenced by cellphone games of the late 90s, with some added features.
---
+++ SnakeGame.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
